63f9927a7dfb4429eea95ace36ee2bf813d465eb
Poom <68049473+poompongphun@users.noreply.github.com>
2023-05-31 10:02:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,38 +3,39 @@ const { getUser, testCase } = require("./mock");
 let done = false; // หากน้องๆทำเสร็จแล้วสามารถเปลี่ยนเป็น true เพื่อเช็คความถูกต้องก่อนเรียกพี่ๆได้เลย
 
 let users = [];
-// ข้อ 1 จงเรียกใช้ getUser และเก็บผลลัพธ์ไว้ในตัวแปร users
-function setUser() {
+// ข้อ 1 จงเรียกใช้ getUser ซึ่งฟังชั่นนี้จะให้ข้อมูล Array ของผู้ใช้กลับมา ภายใน 1 milliseconds
+// แล้วนำข้อมูลผู้ใช้นั้นไปเก็บไว้ในตัวแปรที่ชื่อว่า users (ตัวแปรในบรรทัดที่ 5)
+function initUsers(u) {
   // เพิ่มโค้ดตรงนี้
 
 }
 
 // ข้อ 2
-// จงเขียน Function signIn เมื่อเรียกใช้งาน
-// แล้ว username และ password
-// ถูกต้องมีอยู่ใน Array User ให้ return true
-// หากไม่มีหรือผิดให้ return false
-// หมายเหตุ: username จะไม่เป็น case sensitive
+// จงเขียน Function signIn ซึ่งจะรับ Parameter เป็น username และ password
+// เมื่อเรียกใช้งานแล้ว username และ password ตรงกับข้อมูลที่มีอยู่ใน Array users (ตัวแปรในบรรทัดที่ 5) ให้ return true
+// หากไม่มีอยู่ใน Array users (ตัวแปรในบรรทัดที่ 5) หรือผิดให้ return false
+// หมายเหตุ: username จะไม่เป็น case sensitive และต้องทำข้อที่ 1 ก่อนจึงจะทำข้อนี้ได้
 function signIn(username, password) {
   // เพิ่มโค้ดตรงนี้
 
 }
 
 // ข้อ 3
-// จงเขียน Function signUp เมื่อเรียกใช้งาน
-// ให้เพิ่ม username และ password ใหม่ลงใน Array users
-// โดยความยาวของ password ต้องไม่ต่ำกว่า 3 ตัวและ
-// username ต้องไม่ซ้ำกับข้อมูลที่มีอยู่
-// หากไม่เป็นไปตามเงื่อนไขที่กำหนดจะไม่ถูกเพิ่มแล้ว return false
-// หมายเหตุ: username จะไม่เป็น case sensitive
+// จงเขียน Function signUp ซึ่งจะรับ Parameter เป็น username และ password
+// เมื่อเรียกใช้งานให้นำ username และ password ไปเพิ่มใหม่ลงใน Array users (ตัวแปรในบรรทัดที่ 5)
+// โดยความยาวของ password ต้องไม่ต่ำกว่า 3 ตัวและ username ต้องไม่ซ้ำกับข้อมูลที่มีอยู่แล้วใน Array users
+// หากเพิ่มสำเร็จให้ return true หากไม่เป็นไปตามเงื่อนไขที่กำหนดจะไม่ถูกเพิ่มแล้ว return false
+// หมายเหตุ: username จะไม่เป็น case sensitive และต้องทำข้อที่ 1 ก่อนจึงจะทำข้อนี้ได้
 function signUp(username, password) {
   // เพิ่มโค้ดตรงนี้
 
 }
 
 // ข้อ 4 จงกรองข้อมูลใน Array users โดยให้เอาเฉพาะ username ที่มีคำว่า "it" อยู่ และความยาวมากกว่า 3 ตัวอักษร
-// แล้ว return ค่าเป็น Array ของ username ที่ผ่านเงื่อนไข
-// - (ไม่อยู่ใน TestCase) ข้อ 4.1 ให้ใช้ forEach และ console.log แสดง "ลำดับ", "username", "password" ของแต่ละ user ที่ผ่านเงื่อนไข
+// แล้ว return ค่าเป็น Array ของ users ที่ผ่านเงื่อนไขการกรองแล้ว
+//
+// - ข้อ 4.1 (ไม่อยู่ใน TestCase)
+//   ให้ใช้ forEach และ console.log แสดง "ลำดับ", "ชื่อผู้ใช้", "รหัสผ่าน" ของแต่ละ user ที่ผ่านเงื่อนไขการกรอง
 function filterUsers() {
   // เพิ่มโค้ดตรงนี้
 
